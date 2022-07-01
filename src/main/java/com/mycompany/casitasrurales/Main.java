fca5cb733cab18b07a86b29acafa91be0e48a899
eber <eberchiecher2@gmail.com>
2022-07-01 18:07:16 -0300

    finalizacion

--- src/main/java/com/mycompany/casitasrurales/Main.java
+++ src/main/java/com/mycompany/casitasrurales/Main.java
@@ -13,6 +13,7 @@ public class Main {
         Tabla tablita=new Tabla();
         Scanner input=new Scanner(System.in);
         int n=2;
+        String clave="";///clave del objeto que va a eliminar o buscar 
         String opcionS="";///opcion ingresa como String despues lo transformo en int
         int opcion; 
         presentacion();
@@ -27,10 +28,27 @@ public class Main {
                     
                 break;
                 case 2: 
-                    System.out.println("2");
+                    System.out.println("Dime la clave de la casa a eliminar");
+                    try{
+                        clave= input.nextLine();
+                    
+                    }
+                    catch(Exception e){
+                        System.out.println("Lo sentimos valor incorrecto, vuelvalo a intentar");
+                    }
+                    tablita.eliminar(clave);
                 break;
                 case 3: 
-                    System.out.println("3");
+                    
+                    System.out.println("Dime la clave de la casa que desea buscar");
+                    try{
+                        clave= input.nextLine();
+                    
+                    }
+                    catch(Exception e){
+                        System.out.println("Lo sentimos valor incorrecto, vuelvalo a intentar");
+                    }
+                    tablita.buscar(clave);
                 break;
                 case 4: 
                     n=3;
@@ -59,7 +77,7 @@ public class Main {
     
         System.out.println("1: Ingresar");
         System.out.println("2: Eliminar");
-        System.out.println("3: Mostrar");
+        System.out.println("3: Buscar");
         System.out.println("4: Cerrar el programa");
     }
     
